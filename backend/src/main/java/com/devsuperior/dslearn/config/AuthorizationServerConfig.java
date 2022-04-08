b5a08067d441cb6dd90310f99611e89d0d15917f
Valdemir Vilela Junior <valdemir@softon.com.br>
2022-04-08 00:00:06 -0300

    refresh token

--- backend/src/main/java/com/devsuperior/dslearn/config/AuthorizationServerConfig.java
+++ backend/src/main/java/com/devsuperior/dslearn/config/AuthorizationServerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -45,14 +46,18 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private JwtTokenEnhancer jwtTokenEnhancer;
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
                 .withClient(clientId)
                 .secret(passwordEncoder.encode(clientSecret))
                 .scopes("read", "write")
-                .authorizedGrantTypes("password")
-                .accessTokenValiditySeconds(jwtDuration);
+                .authorizedGrantTypes("password", "refresh_token")
+                .accessTokenValiditySeconds(jwtDuration)
+                .refreshTokenValiditySeconds(jwtDuration);
     }
 
     @Override
@@ -68,6 +73,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         endpoints.authenticationManager(authenticationManager)
                 .tokenStore(jwtTokenStore)
                 .accessTokenConverter(jwtAccessTokenConverter)
-                .tokenEnhancer(chain);
+                .tokenEnhancer(chain)
+                .userDetailsService(userDetailsService);
     }
 }
--- backend/src/main/java/com/devsuperior/dslearn/repositories/NotificationRepository.java
+++ backend/src/main/java/com/devsuperior/dslearn/repositories/NotificationRepository.java
@@ -11,6 +11,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
-    @Query("FROM Notification obj WHERE (obj.user = :user) AND (:unreadOnly = false OR obj.read = false) ORDER BY obj.moment DESC")
+    @Query("SELECT obj FROM Notification obj WHERE (obj.user = :user) AND (:unreadOnly = false OR obj.read = false) ORDER BY obj.moment DESC")
     Page<Notification> find(User user, boolean unreadOnly, Pageable pageable);
 }
